d0286d219ba317c09c82c6c7a857653f31fe8f33
Yannis <yannismadu777@gmail.com>
2023-11-29 00:04:27 +0000

    Add top border to mobile navbar

--- app/components/shared/navbar-parts/MobileNav.tsx
+++ app/components/shared/navbar-parts/MobileNav.tsx
@@ -19,75 +19,77 @@ export default function MobileNav({
   pathname,
 }: NavProps) {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-30 flex justify-between items-end sm:hidden text-lg w-full px-6 py-10 max-w-md mx-auto">
+    <nav className="fixed bottom-0 left-0 right-0 z-30 sm:hidden text-lg w-full px-6 pt-1 pb-4 border-t ">
       <IconContext.Provider value={{ className: "text-3xl -mt-0.5" }}>
-        <Link
-          href={"/"}
-          className={`${
-            pathname === "/"
-              ? "text-onlyfans-black"
-              : "text-onlyfans-light-gray"
-          } ${linkStyles}`}
-          aria-label="Home"
-        >
-          <AiOutlineHome /> <p className="hidden xl:block">Home</p>
-        </Link>
-        <Link
-          href={"/subscriptions"}
-          className={`${
-            pathname === "/subscriptions"
-              ? "text-onlyfans-black"
-              : "text-onlyfans-light-gray"
-          } ${linkStyles}`}
-          aria-label="Subscriptions"
-        >
-          <PiFan />
-        </Link>
-        {pathname === "/create-post" ? (
-          <button
-            disabled
-            className={`${
-              pathname === "/create-post"
-                ? "text-onlyfans-black"
-                : "text-onlyfans-light-gray"
-            } flex items-center gap-5 font-medium max-w-[250px] xl:w-full rounded-full p-2 xl:py-2 xl:px-3 mt-3 transition-colors duration-100`}
-          >
-            <FiPlusSquare className="xl:absolute left-3" />
-          </button>
-        ) : (
+        <div className="max-w-md flex justify-between items-end mx-auto">
           <Link
-            href={`${pathname === "/create-post" ? "" : "/create-post"}`}
+            href={"/"}
             className={`${
-              pathname === "/create-post"
+              pathname === "/"
                 ? "text-onlyfans-black"
                 : "text-onlyfans-light-gray"
             } ${linkStyles}`}
-            aria-label="New Post"
+            aria-label="Home"
           >
-            <FiPlusSquare className="xl:absolute left-3" />
+            <AiOutlineHome /> <p className="hidden xl:block ">Home</p>
           </Link>
-        )}
-        <Link
-          href={"/messages"}
-          className={`${
-            pathname === "/messages"
-              ? "text-onlyfans-black"
-              : "text-onlyfans-light-gray"
-          } ${linkStyles}`}
-        >
-          <BiMessageDetail />
-        </Link>
-        <Link
-          href={"/profile"}
-          className="max-w-[250px] xl:w-full rounded-full focus:outline-onlyfans-blue text-base flex items-center gap-4"
-          aria-label="My Profile"
-        >
-          <Image
-            src={profilePic}
-            alt="Your profile picture"
-            className="w-11 rounded-full"
-          />
-        </Link>
+          <Link
+            href={"/subscriptions"}
+            className={`${
+              pathname === "/subscriptions"
+                ? "text-onlyfans-black"
+                : "text-onlyfans-light-gray"
+            } ${linkStyles}`}
+            aria-label="Subscriptions"
+          >
+            <PiFan />
+          </Link>
+          {pathname === "/create-post" ? (
+            <button
+              disabled
+              className={`${
+                pathname === "/create-post"
+                  ? "text-onlyfans-black"
+                  : "text-onlyfans-light-gray"
+              } flex items-center gap-5 font-medium max-w-[250px] xl:w-full rounded-full p-2 xl:py-2 xl:px-3 mt-3 transition-colors duration-100`}
+            >
+              <FiPlusSquare className="xl:absolute left-3" />
+            </button>
+          ) : (
+            <Link
+              href={`${pathname === "/create-post" ? "" : "/create-post"}`}
+              className={`${
+                pathname === "/create-post"
+                  ? "text-onlyfans-black"
+                  : "text-onlyfans-light-gray"
+              } ${linkStyles}`}
+              aria-label="New Post"
+            >
+              <FiPlusSquare className="xl:absolute left-3" />
+            </Link>
+          )}
+          <Link
+            href={"/messages"}
+            className={`${
+              pathname === "/messages"
+                ? "text-onlyfans-black"
+                : "text-onlyfans-light-gray"
+            } ${linkStyles}`}
+          >
+            <BiMessageDetail />
+          </Link>
+          <Link
+            href={"/profile"}
+            className="max-w-[250px] xl:w-full rounded-full focus:outline-onlyfans-blue text-base flex items-center gap-4 p-2 -mb-0.5"
+            aria-label="My Profile"
+          >
+            <Image
+              src={profilePic}
+              alt="Your profile picture"
+              className="w-10 rounded-full"
+            />
+          </Link>
+        </div>
       </IconContext.Provider>
     </nav>
   );
