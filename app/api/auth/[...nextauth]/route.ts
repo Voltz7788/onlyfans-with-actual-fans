87fff4b9c46386de48dbec7b0813c197e3dbce38
Yannis <yannismadu777@gmail.com>
2023-12-03 22:58:10 +0000

    Add Github and Twitter login with OAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import TwitterProvider from "next-auth/providers/twitter";
+import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/prisma/prismaGlobal";
 
@@ -8,10 +10,19 @@ export const authOptions: NextAuthOptions = {
     signIn: "/login",
   },
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID || "",
+      clientSecret: process.env.TWITTER_CLIENT_SECRET || "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
   ],
   session: {
--- app/components/login-signup/OAuthButtons.tsx
+++ app/components/login-signup/OAuthButtons.tsx
@@ -5,35 +5,31 @@ import Link from "next/link";
 import { signIn } from "next-auth/react";
 
 export default function OAuthButtons() {
-  const handleGoogleSignin = () => {
-    signIn("google");
-  };
-
   return (
     <div className="mb-10 flex flex-col">
-      <Link
-        href={"/"}
+      <button
+        onClick={() => signIn("twitter")}
         className="relative py-2.5 mt-8 rounded-full bg-[#1DA1F2] uppercase w-full text-white text-center text-sm font-semibold hover:opacity-80 transition-all duration-150"
       >
         <TwitterIcon className="absolute w-7 top-1/2 -translate-y-1/2 ml-2" />
         Sign in with Twitter
-      </Link>
+      </button>
 
       <button
-        onClick={handleGoogleSignin}
+        onClick={() => signIn("google")}
         className="relative py-2.5 mt-4 rounded-full bg-[#274a83] uppercase w-full text-white text-center text-sm font-semibold hover:opacity-80 transition-all duration-150"
       >
         <GoogleIcon className="absolute w-7 top-1/2 -translate-y-1/2 ml-2" />
         Sign in with Google
       </button>
 
-      <Link
-        href={"/"}
+      <button
+        onClick={() => signIn("github")}
         className="relative py-2.5 mt-4 rounded-full bg-onlyfans-black uppercase w-full text-white text-center text-sm font-semibold hover:opacity-80 transition-all duration-150"
       >
         <GithubLogo className="absolute w-7 top-1/2 -translate-y-1/2 ml-2" />
         Sign in with Github
-      </Link>
+      </button>
     </div>
   );
 }
