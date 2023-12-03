25acee0a29911b6bcfbbe3454927f4b1ee5549f7
Yannis <yannismadu777@gmail.com>
2023-12-03 16:04:45 +0000

    Add google oauth with prisma adapter

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/prismaGlobal";
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/login",
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/components/login-signup/OAuthButtons.tsx
+++ app/components/login-signup/OAuthButtons.tsx
@@ -1,8 +1,14 @@
+"use client";
 import React from "react";
 import { GithubLogo, GoogleIcon, TwitterIcon } from "../shared/IconComponents";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 export default function OAuthButtons() {
+  const handleGoogleSignin = () => {
+    signIn("google");
+  };
+
   return (
     <div className="mb-10 flex flex-col">
       <Link
@@ -13,13 +19,13 @@ export default function OAuthButtons() {
         Sign in with Twitter
       </Link>
 
-      <Link
-        href={"/"}
+      <button
+        onClick={handleGoogleSignin}
         className="relative py-2.5 mt-4 rounded-full bg-[#274a83] uppercase w-full text-white text-center text-sm font-semibold hover:opacity-80 transition-all duration-150"
       >
         <GoogleIcon className="absolute w-7 top-1/2 -translate-y-1/2 ml-2" />
         Sign in with Google
-      </Link>
+      </button>
 
       <Link
         href={"/"}
