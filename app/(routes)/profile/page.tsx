d6c3659c4fa0f190ab44e9d146cbc64e95d283ca
Yannis <yannismadu777@gmail.com>
2023-12-08 18:47:49 +0000

    Add posts tab on profile page

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "../../utilities/getServerSessionHelper";
 import { redirect } from "next/navigation";
 import ProfileHeader from "@/app/components/profile-page/ProfileHeader";
 
+const demoPosts: any[] = [];
+
 export default async function ProfilePage() {
   const session = await auth();
 
@@ -15,6 +17,31 @@ export default async function ProfilePage() {
     <main className="border-x w-full xl:w-1/3 h-screen">
       <TopNav pageTitle="Profile" session={session} />
       <ProfileHeader />
+      <ProfilePostsContainer posts={demoPosts} />
     </main>
   );
 }
+
+type PostsTabProps = {
+  postsCount: number;
+};
+
+const PostsTab = ({ postsCount }: PostsTabProps) => {
+  return (
+    <div className="p-3 text-center border-b border-black uppercase">
+      <p className="font-medium">
+        {postsCount < 1
+          ? "No Posts"
+          : `${postsCount} ${postsCount === 1 ? "Post" : "Posts"}`}
+      </p>
+    </div>
+  );
+};
+
+type ProfilePostsContainer = {
+  posts: any[];
+};
+
+const ProfilePostsContainer = ({ posts }: ProfilePostsContainer) => {
+  return <PostsTab postsCount={posts.length} />;
+};
